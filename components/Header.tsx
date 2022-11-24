a994b945b4c7cc8d063649c50a58e1e09b888324
josh <josh119891@hotmail.com>
2022-11-24 21:41:36 +0800

    CLEAN

--- components/Header.tsx
+++ components/Header.tsx
@@ -55,7 +55,7 @@ const Header: React.FC<HeaderProps> = ({ placeholder }) => {
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
       <div
-        className="relative flex items-center h-10 cursor-pointer my-auto obj"
+        className="relative flex items-center h-10 cursor-pointer my-auto"
         onClick={() => router.push("/")}
       >
         <Image
